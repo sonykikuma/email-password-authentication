e7d050a52236a32e2c85eb31f0f41f024231c034
abha <abha@example.com>
2022-07-31 15:44:50 +0530

    updated changes

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,17 +7,35 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingTime = adjExpirationTime - currentTime;
+
+  return remainingTime;
+};
+
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = () => {
-    setToken(token);
-  };
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    setTimeout(logoutHandler, remainingTime);
+  };
+
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
